43a4a1f7e26f310a8ff2096606fc2bf4187c6bc5
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-24 21:06:16 +0200

    fix: solve date issue in edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -24,14 +24,14 @@
             <h2 class="my-3">Modifica {{$project->title}}</h2>
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title', $project->title)}}" placeholder="Inserisci il titolo">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title', $project->title )}}" placeholder="Inserisci il titolo">
                 @error('title')
                     <small class="text-danger">{{$message}}</small>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="theme" class="form-label">Argomento</label>
-                <input type="text" class="form-control @error('theme') is-invalid @enderror" id="theme" name="theme" value="{{old('theme', $project->theme)}}" placeholder="Inserisci l'argomento">
+                <input type="text" class="form-control @error('theme') is-invalid @enderror" id="theme" name="theme" value="{{old('theme', $project->theme )}}" placeholder="Inserisci l'argomento">
                 @error('theme')
                     <small class="text-danger">{{$message}}</small>
                 @enderror
@@ -63,14 +63,14 @@
 
             <div class="mb-3">
                 <label for="start_date" class="form-label">Data di inizio</label>
-                <input type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date" name="start_date" value="{{old('start_date', $project->start_date)}}">
+                <input type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date" name="start_date" value="{{old('start_date', $project->start_date ? $project->start_date->format('Y-m-d') : '')}}">
                 @error('start_date')
                     <small class="text-danger">{{$message}}</small>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="end_date" class="form-label">Data di fine</label>
-                <input type="date" class="form-control @error('end_date') is-invalid @enderror" id="end_date" name="end_date" value="{{old('end_date', $project->end_date)}}">
+                <input type="date" class="form-control @error('end_date') is-invalid @enderror" id="end_date" name="end_date" value="{{old('end_date', $project->end_date ? $project->start_date->format('Y-m-d') : '')}}">
                 @error('end_date')
                     <small class="text-danger">{{$message}}</small>
                 @enderror
